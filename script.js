619fd2be906f46c5b1b55369da871890cf14ec6c
EdwinQ12 <quiroz_020927@hotmail.com>
2025-01-31 12:19:49 -0600

    App web de ventilacion inteligente

---
+++ script.js
@@ -0,0 +1,8 @@
+function descargarApp() {
+    alert("Descargando Zefiro...");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Gracias por contactarnos, te responderemos pronto.");
+});
