92328caa8d9a17d131c85b0a485038f3a965cec0
EdwinQ12 <quiroz_020927@hotmail.com>
2025-02-20 13:38:54 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function descargarApp() {
-    alert("Descargando Zefiro...");
-}
-
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Gracias por contactarnos, te responderemos pronto.");
-});
